7e540af5866d9a8c4ca2f4e2a7bd0efd4cf2199b
Vili Sirkeoja <vili.sirkeoja@gmail.com>
2023-03-09 03:00:43 +0200

    Exercise 8.19

--- client/src/components/Books.js
+++ client/src/components/Books.js
@@ -1,4 +1,8 @@
+import { useState } from "react"
+
 const Books = ({ show, books }) => {
+  const [filter, setFilter] = useState(null) // eslint-disable-line
+
   if (!show) {
     return null
   }
@@ -6,6 +10,16 @@ const Books = ({ show, books }) => {
   if (books.loading) {
     return <div>loading...</div>
   }
+
+  console.log(books.data.allBooks)
+  const genres = books.data.allBooks.reduce((acc, book) => {
+    book.genres.forEach((genre) => {
+      if (!acc.includes(genre)) {
+        acc.push(genre)
+      }
+    })
+    return acc
+  }, [])
 
   return (
     <div>
@@ -17,15 +31,29 @@ const Books = ({ show, books }) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.data.allBooks.map((b) => (
-            <tr key={b.title}>
-              <td>{b.title}</td>
-              <td>{b.author.name}</td>
-              <td>{b.published}</td>
-            </tr>
-          ))}
+          {filter
+            ? books.data.allBooks.filter((b) => b.genres.includes(filter)).map((b) => (
+              <tr key={b.title}>
+                <td>{b.title}</td>
+                <td>{b.author.name}</td>
+                <td>{b.published}</td>
+              </tr>
+            ))
+            : books.data.allBooks.map((b) => (
+              <tr key={b.title}>
+                <td>{b.title}</td>
+                <td>{b.author.name}</td>
+                <td>{b.published}</td>
+              </tr>
+            ))
+          }
         </tbody>
       </table>
+      <div>
+        {genres.map(genre =>
+          <button key={genre} onClick={() => setFilter(genre)}>{genre}</button>)}
+          <button onClick={() => setFilter(null)}>all genres</button>
+      </div>
     </div>
   )
 }
